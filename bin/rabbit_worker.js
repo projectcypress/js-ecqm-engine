c6d2a82d7c8ff068c692a9bb944376d2ac928a80
Michael O'Keefe <mokeefe@mitre.org>
2018-05-04 14:30:34 -0400

    Updated rabbit_worker to support both async and sync jobs

--- bin/rabbit_worker.js
+++ bin/rabbit_worker.js
@@ -3,6 +3,10 @@
 const amqp = require('amqplib/callback_api');
 const Executor = require('../lib/executor');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 amqp.connect('amqp://localhost', (err, conn) => {
   conn.createChannel((chErr, ch) => {
     const q = 'calculation_queue';
@@ -17,18 +21,38 @@ amqp.connect('amqp://localhost', (err, conn) => {
     ch.consume(q, (msg) => {
       const messageJSON = JSON.parse(msg.content.toString());
       console.log(messageJSON);
-      executor.execute(messageJSON.patient_ids, messageJSON.measure_ids, messageJSON.options).then(
-        // Success handler
-        (result) => {
-          console.log(`Calculated ${result}`);
-          ch.ack(msg);
-        },
-        // Failure handler
-        (result) => {
-          console.error(result);
-          ch.ack(msg);
-        }
-      );
+      if (messageJSON.type === 'async') {
+        executor.execute(messageJSON.patient_ids, messageJSON.measure_ids, messageJSON.options).then(
+          // Success handler
+          (result) => {
+            console.log(`Calculated ${JSON.stringify(result)}`);
+            ch.ack(msg);
+          },
+          // Failure handler
+          (result) => {
+            console.error(result);
+            ch.ack(msg);
+          }
+        );
+      } else if (messageJSON.type === 'sync') {
+        executor.execute(messageJSON.patient_ids, messageJSON.measure_ids, messageJSON.options).then(
+          // Success handler
+          (result) => {
+            console.log(`Calculated ${JSON.stringify(result)}`);
+            ch.sendToQueue(
+              msg.properties.replyTo,
+              Buffer.from(JSON.stringify(result)),
+              { correlationId: msg.properties.correlationId }
+            );
+            ch.ack(msg);
+          },
+          // Failure handler
+          (result) => {
+            console.error(result);
+            ch.ack(msg);
+          }
+        );
+      }
     }, { noAck: false });
   });
 });
