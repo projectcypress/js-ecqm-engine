02b7b580787e6350fd9c886fd0889d6de33f0d16
Michael O'Keefe <mokeefe@mitre.org>
2018-05-10 11:52:39 -0400

    Added support for environment variables to the rabbit_worker, along with sane defaults if they aren't present

--- bin/rabbit_worker.js
+++ bin/rabbit_worker.js
@@ -4,16 +4,31 @@ const amqp = require('amqplib/callback_api');
 const Executor = require('../lib/executor');
 const mongoose = require('mongoose');
 
-amqp.connect('amqp://localhost', (err, conn) => {
+// Build the RabbitMQ connection URL, with sane defaults if the environment variables don't exist
+const rabbitHost = process.env.RABBITMQ_HOST ? process.env.RABBITMQ_HOST : 'localhost';
+const rabbitPort = process.env.RABBITMQ_PORT ? process.env.RABBITMQ_PORT : '5672';
+const rabbitURL = `amqp://${rabbitHost}:${rabbitPort}`;
+
+// Build the MongoDB connection URL, with sane defaults if the environment variables don't exist
+const mongoHost = process.env.MONGODB_HOST ? process.env.MONGODB_HOST : 'localhost';
+const mongoPort = process.env.MONGODB_HOST ? process.env.MONGODB_PORT : '27017';
+// use NODE_ENV to figure out what environment we're running in (or default to development)
+const cypressEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
+// If we're running in a test environment, we'll need to get the env number to append to the DB
+// If we're not running in a test environment/this variable isn't set, this will be blank and won't matter
+const cypressTestNum = process.env.TEST_ENV_NUMBER ? process.env.TEST_ENV_NUMBER : '';
+const cypressDB = process.env.CYPRESS_DB ? process.env.CYPRESS_DB : `cypress_${cypressEnv}${cypressTestNum}`;
+const mongoURL = `mongodb://${mongoHost}:${mongoPort}/${cypressDB}`;
+
+amqp.connect(rabbitURL, (err, conn) => {
   conn.createChannel((chErr, ch) => {
     const q = 'calculation_queue';
 
     ch.assertQueue(q, { durable: true });
     ch.prefetch(1);
 
-    const connectionURL = 'mongodb://127.0.0.1:27017/cypress_development';
     const connectionOptions = { poolSize: 10 };
-    const connection = mongoose.createConnection(connectionURL, connectionOptions);
+    const connection = mongoose.createConnection(mongoURL, connectionOptions);
 
     process.on('close', conn.close);
 
