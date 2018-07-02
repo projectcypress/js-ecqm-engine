87c59f2a15bbd2df07a1ca62dd304233fe1df3ae
Michael O'Keefe <mokeefe@mitre.org>
2018-07-02 10:46:46 -0400

    Refactor environment variable definition code, and add retries of Rabbit connection with exponential backoff

--- bin/rabbit_worker.js
+++ bin/rabbit_worker.js
@@ -1,99 +1,166 @@
 #!/usr/bin/env node
-
 const amqp = require('amqplib/callback_api');
 const Executor = require('../lib/executor');
 const mongoose = require('mongoose');
+const convict = require('convict');
+const retry = require('retry');
 
-// Build the RabbitMQ connection URL, with sane defaults if the environment variables don't exist
-const rabbitHost = process.env.RABBITMQ_HOST ? process.env.RABBITMQ_HOST : 'localhost';
-const rabbitPort = process.env.RABBITMQ_PORT ? process.env.RABBITMQ_PORT : '5672';
-const rabbitURL = `amqp://${rabbitHost}:${rabbitPort}`;
+const config = convict({
+  env: {
+    doc: 'The application environment',
+    format: ['development', 'production', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+    arg: 'node_env',
+  },
+  rabbitmq: {
+    host: {
+      doc: 'RabbitMQ connection host',
+      format: String,
+      default: 'localhost',
+      env: 'RABBITMQ_HOST',
+      arg: 'rabbitmq_host',
+    },
+    port: {
+      doc: 'RabbitMQ connection port',
+      format: 'port',
+      default: 5672,
+      env: 'RABBITMQ_PORT',
+      arg: 'rabbitmq_port',
+    },
+  },
+  mongodb: {
+    host: {
+      doc: 'MongoDB connection host',
+      format: String,
+      default: 'localhost',
+      env: 'MONGODB_HOST',
+      arg: 'mongodb_host',
+    },
+    port: {
+      doc: 'MongoDB connection port',
+      format: 'port',
+      default: 27017,
+      env: 'MONGODB_PORT',
+      arg: 'mongodb_port',
+    },
+  },
+  cypress: {
+    test_num: {
+      doc: 'Cypress Test number (if test environment',
+      format: '*',
+      default: '',
+      env: 'TEST_NUM',
+      arg: 'test_num',
+    },
+  },
+});
 
-// Build the MongoDB connection URL, with sane defaults if the environment variables don't exist
-const mongoHost = process.env.MONGODB_HOST ? process.env.MONGODB_HOST : 'localhost';
-const mongoPort = process.env.MONGODB_HOST ? process.env.MONGODB_PORT : '27017';
-// use NODE_ENV to figure out what environment we're running in (or default to development)
-const cypressEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
-// If we're running in a test environment, we'll need to get the env number to append to the DB
-// If we're not running in a test environment/this variable isn't set, this will be blank and won't matter
-const cypressTestNum = process.env.TEST_ENV_NUMBER ? process.env.TEST_ENV_NUMBER : '';
-const cypressDB = process.env.CYPRESS_DB ? process.env.CYPRESS_DB : `cypress_${cypressEnv}${cypressTestNum}`;
-const mongoURL = `mongodb://${mongoHost}:${mongoPort}/${cypressDB}`;
+const env = config.get('env');
+config.validate({ allowed: 'strict' });
 
-amqp.connect(rabbitURL, (err, conn) => {
-  conn.createChannel((chErr, ch) => {
-    const q = 'calculation_queue';
+const rabbitURL = `amqp://${config.get('rabbitmq.host')}:${config.get('rabbitmq.port')}`;
+const cypressDB = `cypress_${env}${config.get('cypress.test_num')}`;
+const mongoURL = `mongodb://${config.get('mongodb.host')}` +
+                 `:${config.get('mongodb.port')}/${cypressDB}`;
 
-    ch.assertQueue(q, { durable: true });
-    ch.prefetch(1);
+const operation = retry.operation({
+  retries: 5,
+  minTimeout: 250,
+  factor: 3.5,
+});
 
-    const connectionOptions = { poolSize: 10 };
-    const connection = mongoose.createConnection(mongoURL, connectionOptions);
+operation.attempt(() => {
+  amqp.connect(rabbitURL, (err, conn) => {
+    if (operation.retry(err)) {
+      console.error('No rabbitMQ connection possible, retrying...');
+      return;
+    }
 
-    process.on('close', conn.close);
+    if (!conn) {
+      console.error('No RabbitMQ connection could be made. Please check your RabbitMQ Server/connection settings');
+      return;
+    }
 
-    const executor = new Executor(connection);
-
-    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q);
-
-    ch.consume(q, (msg) => {
-      const messageJSON = JSON.parse(msg.content.toString());
-      // console.log(messageJSON);
-      try {
-        if (messageJSON.type === 'async') {
-          executor.execute(
-            messageJSON.patient_ids,
-            messageJSON.measure_ids,
-            connection,
-            messageJSON.options
-          ).then(
-            // Success handler
-            (result) => {
-              console.log(`Calculated ${JSON.stringify(result)}`);
-              ch.ack(msg);
-            },
-            // Failure handler
-            (result) => {
-              console.error(result);
-              ch.ack(msg);
-            }
-          );
-        } else if (messageJSON.type === 'sync') {
-          executor.execute(
-            messageJSON.patient_ids,
-            messageJSON.measure_ids,
-            connection,
-            messageJSON.options
-          ).then(
-            // Success handler
-            (result) => {
-              console.log(`Calculated ${JSON.stringify(result)}`);
-              ch.sendToQueue(
-                msg.properties.replyTo,
-                Buffer.from(JSON.stringify(result)),
-                { correlationId: msg.properties.correlationId }
-              );
-              ch.ack(msg);
-            },
-            // Failure handler
-            (result) => {
-              console.error(result);
-              ch.ack(msg);
-            }
-          );
-        }
-      } catch (error) {
-        // Uncaught error handler
-        if (messageJSON.type === 'sync') {
-          console.error(error);
-          ch.sendToQueue(
-            msg.properties.replyTo,
-            Buffer.from(JSON.stringify('fail')),
-            { correlationId: msg.properties.correlationId }
-          );
-        }
-        ch.ack(msg);
+    conn.createChannel((chErr, ch) => {
+      if (operation.retry(chErr)) {
+        console.error('Error connecting to channel, retrying...');
+        return;
       }
-    }, { noAck: false });
+
+      const q = 'calculation_queue';
+
+      ch.assertQueue(q, { durable: true });
+      ch.prefetch(1);
+
+      const connectionOptions = { poolSize: 10 };
+      const connection = mongoose.createConnection(mongoURL, connectionOptions);
+
+      process.on('close', conn.close);
+
+      const executor = new Executor(connection);
+
+      console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', q);
+
+      ch.consume(q, (msg) => {
+        const messageJSON = JSON.parse(msg.content.toString());
+        // console.log(messageJSON);
+        try {
+          if (messageJSON.type === 'async') {
+            executor.execute(
+              messageJSON.patient_ids,
+              messageJSON.measure_ids,
+              connection,
+              messageJSON.options
+            ).then(
+              // Success handler
+              (result) => {
+                console.log(`Calculated ${JSON.stringify(result)}`);
+                ch.ack(msg);
+              },
+              // Failure handler
+              (result) => {
+                console.error(result);
+                ch.ack(msg);
+              }
+            );
+          } else if (messageJSON.type === 'sync') {
+            executor.execute(
+              messageJSON.patient_ids,
+              messageJSON.measure_ids,
+              connection,
+              messageJSON.options
+            ).then(
+              // Success handler
+              (result) => {
+                console.log(`Calculated ${JSON.stringify(result)}`);
+                ch.sendToQueue(
+                  msg.properties.replyTo,
+                  Buffer.from(JSON.stringify(result)),
+                  { correlationId: msg.properties.correlationId }
+                );
+                ch.ack(msg);
+              },
+              // Failure handler
+              (result) => {
+                console.error(result);
+                ch.ack(msg);
+              }
+            );
+          }
+        } catch (error) {
+          // Uncaught error handler
+          if (messageJSON.type === 'sync') {
+            console.error(error);
+            ch.sendToQueue(
+              msg.properties.replyTo,
+              Buffer.from(JSON.stringify('fail')),
+              { correlationId: msg.properties.correlationId }
+            );
+          }
+          ch.ack(msg);
+        }
+      }, { noAck: false });
+    });
   });
 });
